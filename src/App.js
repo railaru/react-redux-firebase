6092ed6e302184f4e1bd655bcc1fb7e4e1908b7d
Rutenis <rutenis.raila@nfq.lt>
2020-02-23 16:03:54 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
 
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="App">hello</div>
     </BrowserRouter>
   );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Navbar extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+import { Link } from "react-router-dom";
+
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
+
+const Navbar = () => {
+  return (
+    <nav className="nav nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand-logo">
+          MarioPlan
+        </Link>
+        <SignedInLinks />
+        <SignedOutLinks />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
