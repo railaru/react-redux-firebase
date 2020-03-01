13e48e3cc4dfb0111e8d29580144d2bc236ef3c9
Rutenis <rutenis.raila@nfq.lt>
2020-03-01 15:59:14 +0200

    create reducers

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,17 +6,17 @@ import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 
 const Navbar = () => {
-  return (
-    <nav className="nav nav-wrapper grey darken-3">
-      <div className="container">
-        <Link to="/" className="brand-logo">
-          MarioPlan
-        </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="nav nav-wrapper grey darken-3">
+            <div className="container">
+                <Link to="/" className="brand-logo">
+                    MarioPlan
+                </Link>
+                <SignedInLinks />
+                <SignedOutLinks />
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -3,16 +3,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const SignedOutLinks = () => {
-  return (
-    <ul className="right">
-      <li>
-        <NavLink to="/">Sign In</NavLink>
-      </li>
-      <li>
-        <NavLink to="/">Log In</NavLink>
-      </li>
-    </ul>
-  );
+    return (
+        <ul className="right">
+            <li>
+                <NavLink to="/signup">Sign up</NavLink>
+            </li>
+            <li>
+                <NavLink to="/signin">Log In</NavLink>
+            </li>
+        </ul>
+    );
 };
 
 export default SignedOutLinks;
